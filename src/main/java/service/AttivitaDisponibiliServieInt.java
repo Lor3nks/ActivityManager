ee6add6eaa545205fd5e207b921022d71d581671
david <david@Gov12897.scv.va>
2020-09-30 17:35:47 +0200

    Creazione AttivitaDisponibiliServiceInt

---
+++ src/main/java/service/AttivitaDisponibiliServieInt.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+
+import domain.AttivitaDisponibili;
+
+public interface AttivitaDisponibiliServieInt {
+	public int salvaAttivitaDisponibili(AttivitaDisponibili ad);
+	public int modificaAttivitaDisponibili(AttivitaDisponibili ad);
+	public int modificaAbilitazioneAttivitaDisponibili(AttivitaDisponibili ad);
+	public AttivitaDisponibili recuperaAttivitaDisponibiliById(String id_disp);
+	public List<AttivitaDisponibili>RecuperaAttivitaDisponibili();
+
+}
