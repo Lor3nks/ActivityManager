35e55fb9658f9ece188b02feb7076eb41dd4ca62
Luca <Luca@LAPTOP-QQ6E8G5F.homenet.telecomitalia.it>
2020-09-30 15:59:57 +0200

    modifica interfaccia

--- src/main/java/repository/ImpiegatoDaoInt.java
+++ src/main/java/repository/ImpiegatoDaoInt.java
@@ -1,4 +1,6 @@
-package daoInterface;
+package repository;
+
+import java.util.List;
 
 import domain.Impiegato;
 
@@ -7,11 +9,13 @@ public interface ImpiegatoDaoInt {
 	public int inserisci (Impiegato i);
 	public int modifica (Impiegato i);
 	public int cambiaPwd (String username, String newPassword);
-	public int checkAbilitazione (Impiegato i);
-	public int getAllImpigati ();
-	public int getAllImpByUser (String username);
+	public Boolean checkAbilitazione (Impiegato i);
+	public List <Impiegato> getAllImpiegati ();
+	public Impiegato getImpiegatoByUser (String username);
 	public int modificaAbilitazione(String username, boolean abilitazione);
-	public int checkLogin (String username, String password);
+	public Impiegato checkLogin (String username, String password);
 	
-
+	
+	
+	
 }
