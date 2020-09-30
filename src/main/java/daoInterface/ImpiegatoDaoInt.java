0891ee3c8108e3c733a1ba40f30a3597f3597c7f
Luca <Luca@LAPTOP-QQ6E8G5F.homenet.telecomitalia.it>
2020-09-30 12:19:21 +0200

    impiegato dao interface

--- src/main/java/daoInterface/ImpiegatoDaoInt.java
+++ src/main/java/daoInterface/ImpiegatoDaoInt.java
@@ -1,5 +1,15 @@
 package daoInterface;
 
+import java.sql.Connection;
+
+import domain.Impiegato;
+
 public interface ImpiegatoDaoInt {
+	
+	public int cancella (Connection con, Impiegato i);
+	public int inserisci (Connection con, Impiegato i);
+	public int modifica (Connection con, Impiegato i);
+	public int cambiaPwd (Connection con, Impiegato i);
+	
 
 }
