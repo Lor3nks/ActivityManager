ffd71bffcf9b58158b7ffa4b1d4d0f412e73d272
Lorenzo <plessielle@gmail.com>
2020-09-30 14:48:16 +0200

    modifiche AttDispDaoInt, creaziome AttDispDao

--- src/main/java/daoInterface/AttivitaDisponibiliDaoInt.java
+++ src/main/java/daoInterface/AttivitaDisponibiliDaoInt.java
@@ -5,9 +5,10 @@ import java.util.List;
 import domain.AttivitaDisponibili;
 
 public interface AttivitaDisponibiliDaoInt {
-	public void salva(AttivitaDisponibili ad);
-	public void modifica(AttivitaDisponibili ad);
-	public void getAttivitaDisponibiliById(AttivitaDisponibili ad, String  id_disp);
-	public List<AttivitaDisponibili> getAllAttivitaDisponibili(AttivitaDisponibili ad);
-	public void modificaAbilitazione();
+	public int salva(AttivitaDisponibili ad);
+	public int modifica(AttivitaDisponibili ad);
+	public int modificaAbilitazione(AttivitaDisponibili ad);
+	public AttivitaDisponibili getAttivitaDisponibiliById(String id_disp);
+	public List<AttivitaDisponibili> getAllAttivitaDisponibili();
+	
 }
