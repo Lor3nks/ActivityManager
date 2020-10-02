a8e1dfc2762cb02d661a65030b743d0113e7a9d5
eleon <eleon@DESKTOP-T598DPK.homenet.telecomitalia.it>
2020-10-02 16:38:30 +0200

    Cambio dato ora a LocalTime

--- src/main/java/formatter/LocalTimeFormatter.java
+++ src/main/java/formatter/LocalTimeFormatter.java
@@ -1,34 +1,34 @@
 package formatter;
 
 import java.text.ParseException;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 import org.springframework.format.Formatter;
 
-public class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
+public class LocalTimeFormatter implements Formatter<LocalTime> {
 
     private DateTimeFormatter dateTimeformatter;
     private String timePattern;
 
-    public LocalDateTimeFormatter(String timePattern) {
+    public LocalTimeFormatter(String timePattern) {
         this.timePattern = timePattern;
         dateTimeformatter = DateTimeFormatter.ofPattern(timePattern);
     }
 
     @Override
-    public String print(LocalDateTime date, Locale locale) {
+    public String print(LocalTime date, Locale locale) {
         System.out.println(date.format(dateTimeformatter));
         return date.format(dateTimeformatter);
     }
 
     @Override
-    public LocalDateTime parse(String s, Locale locale) throws ParseException {
+    public LocalTime parse(String s, Locale locale) throws ParseException {
         System.out.println("formatter.parse. s:" + s + ", pattern:" + timePattern);
         try {
-            return LocalDateTime.parse(s, DateTimeFormatter.ofPattern(timePattern));
+            return LocalTime.parse(s, DateTimeFormatter.ofPattern(timePattern));
         } catch (DateTimeParseException e) {
             // the error message will be displayed in <form:errors>
             throw new IllegalArgumentException(
