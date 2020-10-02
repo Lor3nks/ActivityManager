937ed907bc53cc915daac6bea46895d3038f3535
corse <corse@LAPTOP-2JG9K0K5.homenet.telecomitalia.it>
2020-10-02 14:21:20 +0200

    Correzzione AttivitaSvolte

--- src/main/java/domain/AttivitaSvolte.java
+++ src/main/java/domain/AttivitaSvolte.java
@@ -8,35 +8,35 @@ public class AttivitaSvolte implements Serializable {
 
 	private static final long serialVersionUID = 4261989059440444174L;
 	private int id_Trigg;
-	private String username;
-	private String id_Disp;
+	private Impiegato imp;
+	private AttivitaDisponibili att_Disp;
 	private LocalDate data_Attivita;
 	private LocalDateTime ora_Inizio;
 	private LocalDateTime ora_Fine;
 	private String note;
 	
-	
 	public AttivitaSvolte() {
 		super();
+	
 	}
 
-	public AttivitaSvolte(String username, String id_Disp, LocalDate data_Attivita, LocalDateTime ora_Inizio,
-			LocalDateTime ora_Fine, String note) {
+	public AttivitaSvolte(int id_Trigg, Impiegato imp, AttivitaDisponibili att_Disp, LocalDate data_Attivita,
+			LocalDateTime ora_Inizio, LocalDateTime ora_Fine, String note) {
 		super();
-		this.username = username;
-		this.id_Disp = id_Disp;
+		this.id_Trigg = id_Trigg;
+		this.imp = imp;
+		this.att_Disp = att_Disp;
 		this.data_Attivita = data_Attivita;
 		this.ora_Inizio = ora_Inizio;
 		this.ora_Fine = ora_Fine;
 		this.note = note;
 	}
 
-	public AttivitaSvolte(int id_Trigg, String username, String id_Disp, LocalDate data_Attivita, LocalDateTime ora_Inizio,
-			LocalDateTime ora_Fine, String note) {
+	public AttivitaSvolte(Impiegato imp, AttivitaDisponibili att_Disp, LocalDate data_Attivita,
+			LocalDateTime ora_Inizio, LocalDateTime ora_Fine, String note) {
 		super();
-		this.id_Trigg = id_Trigg;
-		this.username = username;
-		this.id_Disp = id_Disp;
+		this.imp = imp;
+		this.att_Disp = att_Disp;
 		this.data_Attivita = data_Attivita;
 		this.ora_Inizio = ora_Inizio;
 		this.ora_Fine = ora_Fine;
@@ -47,25 +47,24 @@ public class AttivitaSvolte implements Serializable {
 		return id_Trigg;
 	}
 
-
 	public void setId_Trigg(int id_Trigg) {
 		this.id_Trigg = id_Trigg;
 	}
 
-	public String getUsername() {
-		return username;
+	public Impiegato getImp() {
+		return imp;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setImp(Impiegato imp) {
+		this.imp = imp;
 	}
 
-	public String getId_Disp() {
-		return id_Disp;
+	public AttivitaDisponibili getAtt_Disp() {
+		return att_Disp;
 	}
 
-	public void setId_Disp(String id_Disp) {
-		this.id_Disp = id_Disp;
+	public void setAtt_Disp(AttivitaDisponibili att_Disp) {
+		this.att_Disp = att_Disp;
 	}
 
 	public LocalDate getData_Attivita() {
@@ -99,6 +98,16 @@ public class AttivitaSvolte implements Serializable {
 	public void setNote(String note) {
 		this.note = note;
 	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+
+
+	
 	
 	
 }
